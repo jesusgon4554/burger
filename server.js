f45e412d5164322f02d01f2504f5ffb52230472f
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-11-04 15:48:46 -0800

    needing index

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
 const express = require("express")
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+//public directory in application
+app.use(express.static("public"));
+
+//parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//set handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//import routes
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes)
+
+//start server
+app.listen(PORT, function(){
+    console.log("Server listening on: http://localhost:" + PORT);
+});
