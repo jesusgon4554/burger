bc9e5c5a2ece45fa0ac8e7ceb6b3f6e86980391c
jesusgon4554 <jesus.gonzalez.jg735@gmail.com>
2020-11-05 11:56:50 -0800

    fixing db bug- table doesnt exist

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,62 @@
-$(document).ready(function(){
-    $(".change-devoured").on("click", function(){
-        var id = $(this).data("id");
-    })
-})
+
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newdevoured = $(this).data("newdevoured");
+  
+      var newdevouredState = {
+        devouredy: newdevoured
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burger/" + id, {
+        type: "PUT",
+        data: newdevouredState
+      }).then(
+        function() {
+          console.log("changed devoured to", newdevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        devouredy: $("[name=devouredy]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burger", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new Burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-Burger").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/burger/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted Burger", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
